0a7ade7278caf47ef0e41693c3b2a39848d94373
Jason Mohabir <jmohabir@stuy.edu>
2016-01-15 10:31:40 -0500

    Started inventory system

---
+++ Item.java
@@ -0,0 +1,92 @@
+package com.projects.aoa;
+
+import static com.projects.aoa.Print.*;
+
+import java.util.*;
+import java.io.*;
+
+class Item {
+
+//Item
+String name, type, category;
+int id;
+int hp, mp, str, def, duration;
+
+//Inventory
+public boolean filled;
+public int count, stackLimit;
+
+static void getAllStats(Item[] e){
+    for(Item i : e){
+        getItemStats(i);
+    }
+}
+
+static void getItemStats(Item i){
+    i.getStats();
+}
+
+void getStats(){
+    try {
+        //System.out.println(System.getProperty("user.dir"));
+
+        FileInputStream fstream = new FileInputStream(System.getProperty("user.dir") 
+                + "/src/com/projects/aoa/" + this.type + "_" + this.name + ".txt");
+
+        DataInputStream in = new DataInputStream(fstream);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String line;
+        int counter = 0;
+
+        while ((line = br.readLine()) != null) {
+            if (line.length() == 0){
+                break;
+            }
+
+            switch (counter) {
+            case 0:
+                this.hp = Integer.parseInt(line);
+                counter++;
+                break;
+            case 1:
+                this.mp = Integer.parseInt(line);
+                counter++;
+                break;
+            case 2:
+                this.def = Integer.parseInt(line);
+                counter++;
+                break;
+            case 3:
+                this.str = Integer.parseInt(line);
+                counter++;
+                break;
+            case 4:
+                this.stackLimit = Integer.parseInt(line);
+                counter++;
+                break;
+            case 5:
+                this.duration = Integer.parseInt(line);
+                counter++;
+                break;
+            }   
+        }
+
+
+        in.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    } 
+}
+
+void printStats(){
+    println("[" + name + "]");
+    println("Type: " + type);
+    println("Duration: " + duration);
+    println("HP:  " + hp);
+    println("MP:  " + mp);
+    println("Def: " + def);
+    println("Str: " + str);
+}
+}
