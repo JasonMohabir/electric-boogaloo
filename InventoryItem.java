01efccc58aca4ecec6f55d84b78b22e687b06c43
JasonM17 <jmohabir@stuy.edu>
2016-01-20 09:21:59 -0500

    Final inventory parts added

---
+++ InventoryItem.java
@@ -0,0 +1,12 @@
+class InventoryItem {
+    public Item item;
+    public int amount;
+    
+    public InventoryItem(Item item, int amount) {
+	this.item = item;
+	this.amount = amount;
+    }
+}
+
+// for shops, people, rooms, etc.
+// keep amounts correct
